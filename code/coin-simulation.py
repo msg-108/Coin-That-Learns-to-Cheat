1e1c7ccf5e22769081333d20586bdb408697fb9c
Madhusudhan Gharti <madhusudhangharti.108@gmail.com>
2026-01-30 01:10:31 +0545

    add window parameter

--- code/coin-simulation.py
+++ code/coin-simulation.py
@@ -5,6 +5,7 @@ N = input ("Total number of tosses: ")
 p = .5 # initial probability of heads
 alpha = .05 #significance level
 k = 5 # update interval
+window = k # window size
 
 # State variables
 recent_outcomes = [] 
